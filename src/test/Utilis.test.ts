048a92926c5aa077195d9dd3fe8f5209c5028e7f
collinsolayemi <collinsolayemi@gmail.com>
2023-02-10 18:27:57 -0800

    implementing more code block

--- src/test/Utilis.test.ts
+++ src/test/Utilis.test.ts
@@ -11,5 +11,24 @@ describe("utilis test suite", () => {
     expect(ParsedUrl.href).toBe("http://localhost:8080/login");
     expect(ParsedUrl.port).toBe("8080");
     expect(ParsedUrl.protocol).toBe("http:");
+    expect(ParsedUrl.query).toEqual({});
+  });
+
+  test("Parse url with query", () => {
+    const ParsedUrl = utilis.parseUrl(
+      "http://localhost:8080/login?user=user&password=pass"
+    );
+    const expectedQuery = {
+      user: "user",
+      password: "pass",
+    };
+    expect(ParsedUrl.query).toEqual(expectedQuery);
+  });
+
+  test("Invalid URL", () => {
+    function expectError() {
+      return utilis.parseUrl("");
+    }
+    expect(expectError).toThrowError();
   });
 });
