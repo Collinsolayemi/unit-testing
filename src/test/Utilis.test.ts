cfc79467359b1b8395d884d8d8ff0332ae23a2b3
collinsolayemi <collinsolayemi@gmail.com>
2023-02-10 17:34:18 -0800

    testing

---
+++ src/test/Utilis.test.ts
@@ -0,0 +1,8 @@
+import { utilis } from "../app/Utilis";
+
+describe("utilis test suite", () => {
+  test("first test", () => {
+    const abc = utilis.toUpperCase("abc");
+    console.log("Test working !!!");
+  });
+});
